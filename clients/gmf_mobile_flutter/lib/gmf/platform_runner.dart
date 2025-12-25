f6bfd78fd7d483d9dc3d513e128821d574141cf3
Pu Justin Scarfy Yang <pujustinyang@gmail.com>
2025-12-25 09:03:55 -0800

    android: foreground service starts/stops with background runner (visibility guarantee)

--- clients/gmf_mobile_flutter/lib/gmf/platform_runner.dart
+++ clients/gmf_mobile_flutter/lib/gmf/platform_runner.dart
@@ -1,11 +1,14 @@
+import 'foreground_android.dart';
 import 'package:flutter/services.dart';
 
 class PlatformRunner {
   static const _ch = MethodChannel("gmf.platform_runner");
   static Future<void> startBackground() async {
+    await GMFForegroundAndroid.start();
     await _ch.invokeMethod("startBackground");
   }
   static Future<void> stopBackground() async {
+    await GMFForegroundAndroid.stop();
     await _ch.invokeMethod("stopBackground");
   }
 }
