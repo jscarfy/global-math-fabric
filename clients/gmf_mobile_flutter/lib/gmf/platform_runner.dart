6f5b73a18d26dcdea040ee916521d6da465daedd
Pu Justin Scarfy Yang <pujustinyang@gmail.com>
2025-12-25 09:05:31 -0800

    runtime: PlatformRunner updates RuntimeStatus on start/stop

--- clients/gmf_mobile_flutter/lib/gmf/platform_runner.dart
+++ clients/gmf_mobile_flutter/lib/gmf/platform_runner.dart
@@ -1,14 +1,17 @@
 import 'foreground_android.dart';
+import 'runtime_status.dart';
 import 'package:flutter/services.dart';
 
 class PlatformRunner {
   static const _ch = MethodChannel("gmf.platform_runner");
   static Future<void> startBackground() async {
     await GMFForegroundAndroid.start();
+    await RuntimeStatus.set(GMFStatus.starting);
     await _ch.invokeMethod("startBackground");
   }
   static Future<void> stopBackground() async {
     await GMFForegroundAndroid.stop();
+    await RuntimeStatus.set(GMFStatus.paused);
     await _ch.invokeMethod("stopBackground");
   }
 }
